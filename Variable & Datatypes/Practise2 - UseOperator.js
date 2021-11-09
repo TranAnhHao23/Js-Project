a6a97354632f3bff171a9d252ff1e41b0c1b1474
TranAnhHao23 <Trananhhaonuce@gmail.com>
2021-11-09 08:17:14 +0700

    Add practise 2 file

---
+++ Variable & Datatypes/Practise2 - UseOperator.js
@@ -0,0 +1,10 @@
+let inputwidth;
+let inputheight;
+inputwidth = prompt("Nhập chiều rộng: ");
+inputheight = prompt("Nhập chiều cao: ");
+let width = parseInt(inputwidth);
+let height = parseInt(inputheight);
+
+let area = width * height;
+
+document.write("Diện tích hình chữ nhật = " + area);
